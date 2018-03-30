8508edb6412ce96b772196de5ca9694a04a098de
Goinmul <21400045@handong.edu>
2018-03-30 15:51:47 +0900

    Now I know how to use git and gradle, gradually tracking my coding. :)

--- src/main/java/Cesco.java
+++ src/main/java/Cesco.java
@@ -1,13 +1,13 @@
 import java.util.Scanner; // importing scanner class
 
 public class Cesco { // This class' name
-	
+
 	public static void main (String args[]) { // main function where the program starts
-		
+
 		// Scanner keyboard1 = new Scanner(System.in); no need to use scanner in main function, so ...
 		Fkiller myKiller = new Fkiller(); // instantiating scanner class to use it
 
 		myKiller.countBug(); // in 'Fkiller' class, there is method 'countBug'. I instantiated it to use it.
-	}
-	
-}
+	}// end of main
+
+} // end of class
--- src/main/java/Fkiller.java
+++ src/main/java/Fkiller.java
@@ -6,42 +6,42 @@ public class Fkiller { // The class name
 	public void countBug() { // Public method countBug.
 
 		Scanner keyboard2 = new Scanner(System.in); // Instantiating scanner.
-		
-		final double GROWTH_RATE = 0.95;
+
+		final double GROWTH_RATE = 0.95; // final makes this variable "Unchangable".
 		final double ONE_BUG_VOLUME = 0.002; // immortal variable! hahaha.
 
 		double population= 0; // a double variable.
 		double totalBugVolume = 0; // a double variable.
 		int count = 0; // // an int variable.
 
-		totalBugVolume = population*ONE_BUG_VOLUME;
+		totalBugVolume = population*ONE_BUG_VOLUME; // literally, totalBugVolume is population times ONE_BUG_VOLUME.
 
-		System.out.println("Enter the total volumes of your house");
-		System.out.print("in cubic feet : ");
-		double houseVolume = keyboard2.nextDouble();
-	
-		System.out.print("roaches in your house : ");
-		double firstRoachNum = keyboard2.nextDouble();
+		System.out.println("Enter the total volumes of your house"); // showing a sentence.
+		System.out.print("in cubic feet : "); // showing a sentence.
+		double houseVolume = keyboard2.nextDouble(); // getting a double number from keyboard(user)
 
-		population = firstRoachNum;
+		System.out.print("roaches in your house : "); // showing a sentence.
+		double firstRoachNum = keyboard2.nextDouble(); // getting a double number from keyboard(user)
 
-		while(totalBugVolume < houseVolume)
+		population = firstRoachNum; // giving the value to 'population' from user('firstRoachNum')
+
+		while(totalBugVolume < houseVolume) // execute while the statement inside is true
 		{
-			double newBugs = population*GROWTH_RATE;
-			double newBugsVolume = newBugs*ONE_BUG_VOLUME;
-			population = population + newBugs;
-			totalBugVolume = totalBugVolume + newBugs;
-			count++;
-			
+			double newBugs = population*GROWTH_RATE; // gives a new value
+			double newBugsVolume = newBugs*ONE_BUG_VOLUME; // also, assigning a new value
+			population = population + newBugs; // same as upper line. giving new value.
+			totalBugVolume = totalBugVolume + newBugs; //  ... this is same too.
+			count++; // count means the weeks taken, since every week is parallel to the while loop's one revolution.
+
 		}
-		System.out.println("Starting with a roach population of : " + (int)firstRoachNum);
-		System.out.println("and a house with a volume of " + (int)houseVolume+ " feet.");
-		System.out.println("after " + count + " weeks,");
-		System.out.println("the house will be filled with " + (int)population+" roaches.");
-		System.out.println("They will fill a volume of " + (int)totalBugVolume+" cubic feet.");
-		System.out.println("Better call Debugging Experts Inc.");
-		
+		System.out.println("Starting with a roach population of : " + (int)firstRoachNum); // showing the results. (int) means type cast. make 'firstRoachNum' purposefully to an int type.
+		System.out.println("and a house with a volume of " + (int)houseVolume+ " feet.");// showing the results.
+		System.out.println("after " + count + " weeks,");// showing the results.
+		System.out.println("the house will be filled with " + (int)population+" roaches.");// showing the results.
+		System.out.println("They will fill a volume of " + (int)totalBugVolume+" cubic feet.");// showing the results.
+		System.out.println("Better call Debugging Experts Inc."); // showing the results.
 
-	}
 
-}
+	} // after finishing coding, ctrl + i for indentation.
+
+} // done! Yeah!
