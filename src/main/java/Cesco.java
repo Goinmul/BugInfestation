ff7121f4ed37b7b56fe05259dea317a15c8a3a9d
Goinmul <21400045@handong.edu>
2018-03-30 11:12:46 +0900

    I uploaded new two classes.

---
+++ src/main/java/Cesco.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class Cesco {
+	
+	public static void main (String args[]) {
+		
+		Scanner keyboard1 = new Scanner(System.in);
+		Fkiller myKiller = new Fkiller();
+
+		myKiller.countBug();
+	}
+	
+}
---
+++ src/main/java/Fkiller.java
@@ -0,0 +1,47 @@
+
+import java.util.Scanner; // importing scanner to use it.
+
+public class Fkiller { // The class name
+
+	public void countBug() { // Public method countBug.
+
+		Scanner keyboard2 = new Scanner(System.in);
+		
+		final double GROWTH_RATE = 0.95;
+		final double ONE_BUG_VOLUME = 0.002; // immortal variable! hahaha.
+
+		double population= 0;
+		double totalBugVolume = 0; // oiewjfoiewf
+		int count = 0;
+
+		totalBugVolume = population*ONE_BUG_VOLUME;
+
+		System.out.println("Enter the total volumes of your house");
+		System.out.print("in cubic feet : ");
+		double houseVolume = keyboard2.nextDouble();
+	
+		System.out.print("roaches in your house : ");
+		double firstRoachNum = keyboard2.nextDouble();
+
+		population = firstRoachNum;
+
+		while(totalBugVolume < houseVolume)
+		{
+			double newBugs = population*GROWTH_RATE;
+			double newBugsVolume = newBugs*ONE_BUG_VOLUME;
+			population = population + newBugs;
+			totalBugVolume = totalBugVolume + newBugs;
+			count++;
+			
+		}
+		System.out.println("Starting with a roach population of : " + (int)firstRoachNum);
+		System.out.println("and a house with a volume of " + (int)houseVolume+ " feet.");
+		System.out.println("after " + count + " weeks,");
+		System.out.println("the house will be filled with " + (int)population+" roaches.");
+		System.out.println("They will fill a volume of " + (int)totalBugVolume+" cubic feet.");
+		System.out.println("Better call Debugging Experts Inc.");
+		
+
+	}
+
+}
